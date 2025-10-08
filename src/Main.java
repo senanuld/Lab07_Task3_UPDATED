541a23c80ed765fd20953665ea839a86e600684f
Leonard Senanu <senanuld@mail.uc.edu>
2025-10-07 23:00:26 -0400

    Lab07_Task3 Completed

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("loop 3 count up by 3 from 0 to 18: ");
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                for (int k = 1; k <= 3; k++) {
+
+                }
+                System.out.println(i + "");
+            }
+
+        }
+
+    }
+}
